1227651dc06162b41e7661d8b317c616d3b7f65d
urawa72 <urawa72@gmail.com>
2020-11-15 15:10:15 +0900

    make scripts for bulk insert

---
+++ src/bulk-index.ts
@@ -0,0 +1,21 @@
+import * as Es from '@elastic/elasticsearch';
+import { createReadStream } from 'fs';
+import split from 'split2';
+
+const client = new Es.Client({
+  node: 'http://localhost:9200',
+});
+
+const bulk = async () => {
+  const result = await client.helpers.bulk({
+    datasource: createReadStream('output.ndjson').pipe(split()),
+    onDocument() {
+      return {
+        index: { _index: 'my-test' },
+      };
+    },
+  });
+  console.log(result);
+};
+
+bulk().catch(console.log);
---
+++ src/create-index.ts
@@ -0,0 +1,30 @@
+import * as Es from '@elastic/elasticsearch';
+import { DateTime } from 'luxon';
+
+const client = new Es.Client({
+  node: 'http://localhost:9200',
+});
+
+const createDoc = async () => {
+  const sleep = () => new Promise((resolve) => setTimeout(resolve, 1000));
+  let idx = 18000;
+  const loop = true;
+  while (loop) {
+    idx++;
+    let temp = Math.round((Math.random() * (35 - 20) + 20) * 10000) / 10000;
+    if (idx % 100 === 0) temp = 100;
+    const param = {
+      id: idx,
+      temperature: temp,
+      timestamp: DateTime.local().toISO(),
+    };
+    console.log('Create Doc:', JSON.stringify(param));
+    await client.index({
+      index: 'my-test',
+      body: param,
+    });
+    await sleep();
+  }
+};
+
+createDoc().catch(console.log);
